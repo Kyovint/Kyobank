0bbbdeb3d7369869b926b2895bda85f94ebb1c94
Kyovint <kevinardila.as@gmail.com>
2024-01-18 20:12:40 -0500

    Copyright creado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("bienvenido al programa de nuevo");
         Menu.showMainMenu();
+        System.out.println("Creado por Kevin Ardila");
     }
 }
