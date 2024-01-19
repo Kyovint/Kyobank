0d7c78a04e5b949b0e15879a1d02d7d94aac8495
Kyovint <kevinardila.as@gmail.com>
2024-01-18 22:39:42 -0500

    año futuro

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("bienvenido al programa de nuevo, estimado operador");
         Menu.showMainMenu();
         System.out.println("Creado por Kevin Ardila");
-        System.out.println("Año 2024");
+        System.out.println("Año 2025 para el futuro");
     }
 }
