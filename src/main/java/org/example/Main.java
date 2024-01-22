b68d96f125fb8ef7006f284cdcff140da0a27134
Kevin Ardila <48103810+Kyovint@users.noreply.github.com>
2024-01-22 14:32:19 -0500

    Menu principal title

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("bienvenido al programa de nuevo, estimado operador");
+        System.out.println("---MENU PRINCIPAL---");
         Menu.showMainMenu();
         System.out.println("Creado por Kevin Ardila");
         System.out.println("Enero 18 de 2024");
